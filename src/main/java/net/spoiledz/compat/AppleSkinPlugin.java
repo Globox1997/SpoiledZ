90630c03d987d9c26d7e027b20e2eb48dfae54a0
Globox1997 <mitschke11@web.de>
2023-03-27 09:39:47 +0200

    marked as unused

--- src/main/java/net/spoiledz/compat/AppleSkinPlugin.java
+++ src/main/java/net/spoiledz/compat/AppleSkinPlugin.java
@@ -3,37 +3,39 @@ package net.spoiledz.compat;
 import squeek.appleskin.api.AppleSkinApi;
 import squeek.appleskin.api.event.FoodValuesEvent;
 
-public class AppleSkinPlugin implements AppleSkinApi{
-	@Override
-	public void registerEvents() {
-		FoodValuesEvent.EVENT.register(event -> {
-			 if (event.player != null) {
-			// 	ItemStack stack = event.itemStack;
-			// 	if (FabricLoader.getInstance().isModLoaded("polymer")) {
-			// 		Identifier polymerId = SoFPolymer.getPolymerItemId(event.itemStack);
-			// 		if (polymerId != null && polymerId.getNamespace().equals(SpiceOfFabric.MOD_ID)) {
-			// 			Item item = SoFPolymer.getPolymerItem(polymerId);
-			// 			if (item instanceof FoodContainerItem containerItem) {
-			// 				NbtCompound nbt = SoFPolymer.getPolymerTag(stack.getOrCreateNbt());
-			// 				stack = new ItemStack(item);
-			// 				stack.setNbt(nbt);
-			// 				stack = processFoodContainer(event, stack, containerItem);
-			// 			}
-			// 		}
-			// 	}
+//Unused since capsaicin is used
+@SuppressWarnings("unused")
+public class AppleSkinPlugin implements AppleSkinApi {
+    @Override
+    public void registerEvents() {
+        // FoodValuesEvent.EVENT.register(event -> {
+        // if (event.player != null) {
+        // ItemStack stack = event.itemStack;
+        // if (FabricLoader.getInstance().isModLoaded("polymer")) {
+        // Identifier polymerId = SoFPolymer.getPolymerItemId(event.itemStack);
+        // if (polymerId != null && polymerId.getNamespace().equals(SpiceOfFabric.MOD_ID)) {
+        // Item item = SoFPolymer.getPolymerItem(polymerId);
+        // if (item instanceof FoodContainerItem containerItem) {
+        // NbtCompound nbt = SoFPolymer.getPolymerTag(stack.getOrCreateNbt());
+        // stack = new ItemStack(item);
+        // stack.setNbt(nbt);
+        // stack = processFoodContainer(event, stack, containerItem);
+        // }
+        // }
+        // }
 
-			// 	if (stack.getItem() instanceof FoodContainerItem containerItem) {
-			// 		stack = processFoodContainer(event, stack, containerItem);
-			// 	}
+        // if (stack.getItem() instanceof FoodContainerItem containerItem) {
+        // stack = processFoodContainer(event, stack, containerItem);
+        // }
 
-			// 	int hungerValue = event.modifiedFoodValues.hunger;
-			// 	float saturationValue = event.modifiedFoodValues.saturationModifier;
-			// 	Config.setHungerExpressionValues(
-			// 			((IHungerManager) event.player.getHungerManager()).spiceOfFabric_getFoodHistory().getTimesEaten(stack),
-			// 			hungerValue, saturationValue, event.itemStack.getMaxUseTime()
-			// 	);
-			// 	event.modifiedFoodValues = new FoodValues(Config.getHungerValue(), Config.getSaturationValue());
-			 }
-		});
-	}
+        // int hungerValue = event.modifiedFoodValues.hunger;
+        // float saturationValue = event.modifiedFoodValues.saturationModifier;
+        // Config.setHungerExpressionValues(
+        // ((IHungerManager) event.player.getHungerManager()).spiceOfFabric_getFoodHistory().getTimesEaten(stack),
+        // hungerValue, saturationValue, event.itemStack.getMaxUseTime()
+        // );
+        // event.modifiedFoodValues = new FoodValues(Config.getHungerValue(), Config.getSaturationValue());
+        // }
+        // });
+    }
 }
