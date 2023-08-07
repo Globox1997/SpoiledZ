afc6fab67cb6ffea7d032ae40e99968a8008bba5
Globox1997 <mitschke11@web.de>
2023-08-07 14:34:45 +0200

    fixed farmers delight compat

--- src/main/java/net/spoiledz/mixin/compat/CookingPotBlockEntityMixin.java
+++ src/main/java/net/spoiledz/mixin/compat/CookingPotBlockEntityMixin.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.nhoryzon.mc.farmersdelight.entity.block.CookingPotBlockEntity;
 import com.nhoryzon.mc.farmersdelight.entity.block.SyncedBlockEntity;
-import com.nhoryzon.mc.farmersdelight.entity.block.inventory.CookingPotInventory;
 import com.nhoryzon.mc.farmersdelight.recipe.CookingPotRecipe;
 
 import org.spongepowered.asm.mixin.Final;
@@ -15,10 +14,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.spoiledz.util.SpoiledUtil;
 
@@ -28,16 +28,16 @@ public abstract class CookingPotBlockEntityMixin extends SyncedBlockEntity {
     @Shadow
     @Mutable
     @Final
-    private CookingPotInventory inventory;
+    private DefaultedList<ItemStack> inventory;
 
     public CookingPotBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
 
-    @Inject(method = "processCooking", at = @At(value = "INVOKE", target = "Lcom/nhoryzon/mc/farmersdelight/entity/block/inventory/CookingPotInventory;setStack(ILnet/minecraft/item/ItemStack;)V", ordinal = 0, shift = Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT)
+    @Inject(method = "processCooking", at = @At(value = "INVOKE", target = "Lcom/nhoryzon/mc/farmersdelight/entity/block/CookingPotBlockEntity;setStack(ILnet/minecraft/item/ItemStack;)V", ordinal = 0, shift = Shift.AFTER))
     private void processCookingMixin(CookingPotRecipe recipe, CallbackInfoReturnable<Boolean> info) {
-        SpoiledUtil.setItemStackSpoilage(world, this.inventory.getStack(6),
-                List.of(this.inventory.getStack(0), this.inventory.getStack(1), this.inventory.getStack(2), this.inventory.getStack(3), this.inventory.getStack(4), this.inventory.getStack(5)));
+        SpoiledUtil.setItemStackSpoilage(world, this.inventory.get(6),
+                List.of(this.inventory.get(0), this.inventory.get(1), this.inventory.get(2), this.inventory.get(3), this.inventory.get(4), this.inventory.get(5)));
     }
 
 }
